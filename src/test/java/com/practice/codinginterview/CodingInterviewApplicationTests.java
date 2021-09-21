b660ca815713ec0d80ef4a980603bc608c0c725f
kavithasimac <kavithasubbiah@hotmail.com>
2021-09-20 20:34:02 -0400

    Initial commit

---
+++ src/test/java/com/practice/codinginterview/CodingInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.codinginterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
